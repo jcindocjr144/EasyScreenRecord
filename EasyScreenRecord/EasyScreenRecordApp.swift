fdb5b35380a788b0c62ba31e08a7fa4dd121fc77
Naoki Takahashi / 電電猫猫 <43579605+nyanko3141592@users.noreply.github.com>
2026-01-08 16:18:40 +0900

    Initial Commit

---
+++ EasyScreenRecord/EasyScreenRecordApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyScreenRecordApp.swift
+//  EasyScreenRecord
+//
+//  Created by 高橋直希 on 2026/01/08.
+//
+
+import SwiftUI
+
+@main
+struct EasyScreenRecordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
